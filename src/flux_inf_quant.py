0b5893b5d959870ea41582236b2110f3ec22c17e
Aishwary Mishra <aiswary.8109@gmail.com>
2024-12-22 22:08:34 +0530

    files path changed

--- src/flux_inf_quant.py
+++ src/flux_inf_quant.py
@@ -35,8 +35,8 @@ def load_object_from_file(filename):
 
 
 
-def inference_sample(id, request_id, lora_weight, prompt, ckpt_4bit_id="/workspace/flux.1-dev-nf4-pkg",
-                     ckpt_id="/workspace/flux-dev-1"):
+def inference_sample(id, request_id, lora_weight, prompt, ckpt_4bit_id="/runpod-volume/flux-dev-1-nf4",
+                     ckpt_id="/runpod-volume/FLUX.1-dev"):
     """This function is used to create sample using flux model"""
     flush()
     logger.info(f"{lora_weight} #########")
